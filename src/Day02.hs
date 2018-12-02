4bfd4228a94883793390753a0a33f6be9b6716cf
Jonathan Chan <afnl686@gmail.com>
2018-12-02 09:28:02 -0800

    Day 02 - (\) deletes the first occurrence, so back to using zip + filter.

--- src/Day02.hs
+++ src/Day02.hs
@@ -7,7 +7,8 @@ part1 strs = (length . filter (count 2) $ strs) * (length . filter (count 3) $ s
     where count n = any (== n) . map length . group . sort
 
 part2 :: [String] -> String
-part2 strs = head [ls `intersect` rs | ls <- strs, rs <- strs, length (ls \\ rs) == 1]
+part2 strs = head [xs `intersect` ys | xs <- strs, ys <- strs, dist xs ys == 1]
+    where dist = (length .) . (filter id .) . zipWith (/=)
 
 main :: IO ()
 main = do
