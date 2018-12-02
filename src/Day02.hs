3e1e067dc866b9ac69092f0fad1fbbb15342bb4e
Jonathan Chan <afnl686@gmail.com>
2018-12-01 23:14:27 -0800

    Day 02.

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,16 @@
 module Day02 (main) where
 
+import Data.List (group, sort, (\\), intersect)
+
+part1 :: [String] -> Int
+part1 strs = (length . filter (count 2) $ strs) * (length . filter (count 3) $ strs)
+    where count n = any (== n) . map length . group . sort
+
+part2 :: [String] -> String
+part2 strs = head [ls `intersect` rs | ls <- strs, rs <- strs, length (ls \\ rs) == 1]
+
 main :: IO ()
 main = do
-    input <- readFile "input/02.txt"
-    print input
+    input <- lines <$> readFile "input/02.txt"
+    print    $ part1 input
+    putStrLn $ part2 input
