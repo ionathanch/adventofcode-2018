33f9ddcaebb9e86815b9e663ad644e3a08fb2377
Jonathan Chan <afnl686@gmail.com>
2018-12-05 11:53:46 -0800

    Day 05.

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,6 +1,17 @@
 module Day05 (main) where
 
+import Data.Char (toLower)
+
+part1 :: String -> Int
+part1 = length . foldr react []
+    where react c [] = [c]
+          react c (u:us) = if (c /= u) && (toLower c == toLower u) then us else (c:u:us)
+
+part2 :: String -> Int
+part2 str = minimum . map (\c -> part1 . filter ((/= c) . toLower) $ str) $ ['a'..'z']
+
 main :: IO ()
 main = do
     input <- readFile "input/05.txt"
-    print input
+    print $ part1 input
+    print $ part2 input
